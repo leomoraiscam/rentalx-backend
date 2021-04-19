aa6f0793deb0c6b97fa416073d343559a8919bd1
lmorais <leozonnn@gmail.com>
2021-04-19 16:32:01 -0300

    configuration sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,6 +3,10 @@ import 'dotenv/config';
 import cors from 'cors';
 import express, { Response, Request, NextFunction } from 'express';
 import swaggerUi from 'swagger-ui-express';
+
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
+
 import 'express-async-errors';
 
 import '../../container';
@@ -19,12 +23,23 @@ createConnection();
 const app = express();
 
 app.use(rateLimiter);
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/car', express.static(`${upload.tmpFolder}/cars`));
 app.use(cors());
 app.use(routes);
+app.use(Sentry.Handlers.errorHandler());
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
