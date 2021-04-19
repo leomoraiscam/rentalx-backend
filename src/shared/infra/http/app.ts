5ee353ccf85cbf660263c3a47f48d15b2036e581
lmorais <leozonnn@gmail.com>
2021-04-19 16:10:04 -0300

    configuration redis limiter

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -12,11 +12,13 @@ import AppError from '@shared/errors/AppError';
 
 import swaggerFile from '../../../swagger.json';
 import createConnection from '../typeorm';
+import rateLimiter from './middlewares/rateLimiter';
 import routes from './routes';
 
 createConnection();
 const app = express();
 
+app.use(rateLimiter);
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,10 +1,11 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (): Promise<Connection> => {
+export default async (host = 'database'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
+      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
       database:
         process.env.NODE_ENV === 'test'
           ? 'rentx_test'
