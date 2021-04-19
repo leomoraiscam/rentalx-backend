d636acfe8d8a81755c9fd7d36f56008fe458b26e
lmorais <leozonnn@gmail.com>
2021-04-19 16:48:47 -0300

    add password for redis

--- src/shared/infra/http/middlewares/rateLimiter.ts
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -7,6 +7,7 @@ import AppError from '@shared/errors/AppError';
 const redisClient = redis.createClient({
   host: process.env.REDIS_HOST,
   port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASS || undefined,
 });
 
 const limiter = new RateLimiterRedis({
