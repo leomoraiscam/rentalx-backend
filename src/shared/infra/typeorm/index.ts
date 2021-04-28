1977e6a46818686f3c14bb9c9f7ff8f1fea40e6e
lmorais <leozonnn@gmail.com>
2021-04-27 21:48:24 -0300

    feat: update pipeline

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -5,11 +5,23 @@ export default async (host = process.env.PG_HOST): Promise<Connection> => {
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
+      host: process.env.NODE_ENV === 'test' ? process.env.PG_HOST_TEST : host,
+      username:
+        process.env.NODE_ENV === 'test'
+          ? process.env.PG_USERNAME_TEST
+          : process.env.PG_USERNAME,
+      password:
+        process.env.NODE_ENV === 'test'
+          ? process.env.PG_PASSWORD_TEST
+          : process.env.PG_PASSWORD,
       database:
         process.env.NODE_ENV === 'test'
-          ? 'rentx_test'
+          ? process.env.PG_DATABASE_TEST
           : defaultOptions.database,
+      port:
+        process.env.NODE_ENV === 'test'
+          ? process.env.PG_PORT_TEST
+          : process.env.PG_PORT,
     })
   );
 };
