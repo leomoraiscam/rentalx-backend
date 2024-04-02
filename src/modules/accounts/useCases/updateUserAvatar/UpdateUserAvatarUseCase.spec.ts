bfd2f97680b58b1b654d6acf3a4ad92ad462a087
lmorais <lmorais.contato@gmail.com>
2024-04-02 16:25:22 -0300

    test: added unit test to update useravatar service

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.spec.ts
@@ -0,0 +1,68 @@
+import { InMemoryStorageProvider } from '@shared/container/providers/StorageProvider/in-memory/InMemoryStorageProvider';
+import AppError from '@shared/errors/AppError';
+
+import { InMemoryUserRepository } from '../../repositories/in-memory/InMemoryUserRepository';
+import { UpdateUserAvatarUseCase } from './UpdateUserAvatarUseCase';
+
+let inMemoryUserRepository: InMemoryUserRepository;
+let inMemoryStorageProvider: InMemoryStorageProvider;
+let updateUserAvatarUseCase: UpdateUserAvatarUseCase;
+
+describe('UpdateUserAvatarUseCase', () => {
+  beforeEach(() => {
+    inMemoryUserRepository = new InMemoryUserRepository();
+    inMemoryStorageProvider = new InMemoryStorageProvider();
+    updateUserAvatarUseCase = new UpdateUserAvatarUseCase(
+      inMemoryUserRepository,
+      inMemoryStorageProvider
+    );
+  });
+
+  it('should be able to update avatar a user', async () => {
+    const user = await inMemoryUserRepository.create({
+      name: 'Richard Thomas',
+      email: 'ana@ru.sv',
+      password: 'any-password@123',
+      driverLicense: '8364802278',
+    });
+
+    const { id: userId } = user;
+
+    await updateUserAvatarUseCase.execute({
+      userId,
+      avatarFile: 'avatar.jpg',
+    });
+
+    expect(user.avatar).toBe('avatar.jpg');
+  });
+
+  it('should not be able to update avatar when a non existing user', async () => {
+    await expect(
+      updateUserAvatarUseCase.execute({
+        userId: 'non-existing-user',
+        avatarFile: 'avatar.jpg',
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to delete old avatar file when updating user avatar', async () => {
+    const avatarFile = 'avatar-file.jpg';
+
+    const deleteFileSpied = jest.spyOn(inMemoryStorageProvider, 'delete');
+
+    const { id: userId } = await inMemoryUserRepository.create({
+      name: 'Cody Reid',
+      email: 'weppute@lubpep.cu',
+      password: 'password@123',
+      driverLicense: '6878209902',
+      avatar: 'old-avatar-file.jpg',
+    });
+
+    await updateUserAvatarUseCase.execute({ userId, avatarFile });
+
+    expect(deleteFileSpied).toHaveBeenCalledTimes(1);
+    expect(deleteFileSpied).toHaveBeenCalledWith(
+      `./tmp/avatar/old-avatar-file.jpg`
+    );
+  });
+});
