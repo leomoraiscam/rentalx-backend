0b9295c2fd86d294cbf0e92b6c4f57d5bb9e15c1
lmorais <lmorais.contato@gmail.com>
2024-04-02 14:44:51 -0300

    test: added unit test to create user service

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,67 @@
+import { InMemoryUserRepository } from '@modules/accounts/repositories/in-memory/InMemoryUserRepository';
+import { InMemoryHashProvider } from '@shared/container/providers/HashProvider/in-memory/InMemoryHashProvider';
+import AppError from '@shared/errors/AppError';
+
+import { CreateUserUseCase } from './CreateUserUseCase';
+
+let inMemoryUserRepository: InMemoryUserRepository;
+let inMemoryHashProvider: InMemoryHashProvider;
+let createUserUseCase: CreateUserUseCase;
+
+describe('CreateUserUseCase', () => {
+  beforeEach(() => {
+    inMemoryUserRepository = new InMemoryUserRepository();
+    inMemoryHashProvider = new InMemoryHashProvider();
+    createUserUseCase = new CreateUserUseCase(
+      inMemoryUserRepository,
+      inMemoryHashProvider
+    );
+  });
+
+  it('should be able to create a new user when received correct data', async () => {
+    const user = await createUserUseCase.execute({
+      name: 'Claudia Sharp',
+      email: 'nettu@cochu.bg',
+      password: 'pass@123',
+      driverLicense: '7545472319',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user when already exist email for a user', async () => {
+    await inMemoryUserRepository.create({
+      name: 'Teresa Hammond',
+      email: 'fal@kecahpos.jm',
+      password: 'pass123',
+      driverLicense: '9118477562',
+    });
+
+    await expect(
+      createUserUseCase.execute({
+        name: 'Stella Bradley',
+        email: 'fal@kecahpos.jm',
+        password: 'pass123',
+        driverLicense: '7203294633',
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create a new user when already exist driver license for a user', async () => {
+    await inMemoryUserRepository.create({
+      name: 'Jennie McCoy',
+      email: 'hiv@metja.lk',
+      password: 'any-pass123',
+      driverLicense: '5304286925',
+    });
+
+    await expect(
+      createUserUseCase.execute({
+        name: 'Sue Rogers',
+        email: 'guwda@ih.za',
+        password: 'any-pass123',
+        driverLicense: '5304286925',
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
