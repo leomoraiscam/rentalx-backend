b2511e418cacbbfe5a5ed7758b7a0d33fe6b5d04
lmorais <lmorais.contato@gmail.com>
2024-04-04 17:52:55 -0300

    fix: import correctly resource

--- src/modules/cars/dtos/ICreateCarDTO.ts
+++ src/modules/cars/dtos/ICreateCarDTO.ts
@@ -1,4 +1,4 @@
-import Specification from '../infra/typeorm/entities/Specification';
+import { Specification } from '../infra/typeorm/entities/Specification';
 
 export interface ICreateCarDTO {
   id?: string;
