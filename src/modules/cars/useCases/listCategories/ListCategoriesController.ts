5f284c99535a181f802a624a48c49c87b5c5ffe7
lmorais <lmorais.contato@gmail.com>
2024-04-09 10:44:26 -0300

    feat: update parameters in list categories controller

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,12 +1,20 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
+import { OrdenationProps } from '@modules/cars/dtos/IQueryListCategoriesDTO';
+
 import { ListCategoriesUseCase } from './ListCategoriesUseCase';
 
 export class ListCategoriesController {
   async handle(request: Request, response: Response): Promise<Response> {
+    const { page, perPage, order } = request.query;
+
     const listCategoriesUseCase = container.resolve(ListCategoriesUseCase);
-    const categories = await listCategoriesUseCase.execute();
+    const categories = await listCategoriesUseCase.execute({
+      order: order as OrdenationProps,
+      page: Number(page),
+      perPage: Number(perPage),
+    });
 
     return response.status(200).json(categories);
   }
