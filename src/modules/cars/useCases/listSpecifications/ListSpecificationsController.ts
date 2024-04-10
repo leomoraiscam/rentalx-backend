551fa681e9f3bacbae1c6fd623ede363de89d04b
lmorais <lmorais.contato@gmail.com>
2024-04-10 11:02:12 -0300

    feat: add specifications list with pagination parameters

---
+++ src/modules/cars/useCases/listSpecifications/ListSpecificationsController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { OrdenationProps } from '@modules/cars/dtos/IQueryListCategoriesDTO';
+
+import { ListSpecificationsUseCase } from './ListSpecificationsUseCase';
+
+export class ListSpecificationsController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { page, perPage, order } = request.query;
+
+    const listSpecificationsUseCase = container.resolve(
+      ListSpecificationsUseCase
+    );
+    const specifications = await listSpecificationsUseCase.execute({
+      order: order as OrdenationProps,
+      page: Number(page),
+      perPage: Number(perPage),
+    });
+
+    return response.status(200).json(specifications);
+  }
+}
--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,12 +1,19 @@
 import { Router } from 'express';
 
 import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/CreateSpecificationController';
+import { ListSpecificationsController } from '@modules/cars/useCases/listSpecifications/ListSpecificationsController';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const specificationRouter = Router();
 const createSpecificationController = new CreateSpecificationController();
+const listSpecificationsController = new ListSpecificationsController();
 
+specificationRouter.get(
+  '/',
+  ensureAuthenticated,
+  listSpecificationsController.handle
+);
 specificationRouter.post(
   '/',
   ensureAuthenticated,
