43fdf0ae80a07321c0bfa19a190a00436cca7faa
lmorais <lmorais.contato@gmail.com>
2024-04-28 21:43:40 -0300

    feat: add type field in layers

--- src/modules/cars/dtos/ICreateCategoryDTO.ts
+++ src/modules/cars/dtos/ICreateCategoryDTO.ts
@@ -1,4 +1,11 @@
+export enum CategoryType {
+  HATCH = 'hatch',
+  SEDAN = 'sedan',
+  SUV = 'suv',
+}
+
 export interface ICreateCategoryDTO {
   name: string;
   description: string;
+  type: CategoryType;
 }
--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -5,12 +5,13 @@ import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
 export class CreateCategoryController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { name, description } = request.body;
+    const { name, description, type } = request.body;
 
     const createCategoryUseCase = container.resolve(CreateCategoryUseCase);
     const category = await createCategoryUseCase.execute({
       name,
       description,
+      type,
     });
 
     return response.status(201).json(category);
