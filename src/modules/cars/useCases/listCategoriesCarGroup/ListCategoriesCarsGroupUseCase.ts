f62297d51e973253f4b3ae7b0aaece69a21fd101
lmorais <lmorais.contato@gmail.com>
2024-05-01 15:55:25 -0300

    feat: apply brand and type fields

--- src/modules/cars/useCases/listCategoriesCarGroup/ListCategoriesCarsGroupUseCase.ts
+++ src/modules/cars/useCases/listCategoriesCarGroup/ListCategoriesCarsGroupUseCase.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 import { IListCategoriesCarsGroupDTO } from '@modules/cars/dtos/IListCategoriesCarsGroupDTO';
+import { IQueryListAvailableCarsDTO } from '@modules/cars/dtos/IQueryListAvailableCarsDTO';
 import { OrdenationProps } from '@modules/cars/dtos/IQueryListCategoriesDTO';
 import { ICarRepository } from '@modules/cars/repositories/ICarRepository';
 import { ICategoryRepository } from '@modules/cars/repositories/ICategoryRepository';
@@ -17,13 +18,15 @@ export class ListCategoriesCarsGroupUseCase {
     private rentalRepository: IRentalRepository
   ) {}
 
-  async execute(): Promise<IListCategoriesCarsGroupDTO[]> {
+  async execute(
+    data?: IQueryListAvailableCarsDTO
+  ): Promise<IListCategoriesCarsGroupDTO[]> {
     const categories = await this.categoryRepository.list({
       order: OrdenationProps.DESC,
       page: 1,
       perPage: 5,
     });
-    const cars = await this.carRepository.findAvailable({});
+    const cars = await this.carRepository.findAvailable();
 
     const transformedCategories = categories.result.map(async (category) => {
       const categoryCars = cars
@@ -55,6 +58,24 @@ export class ListCategoriesCarsGroupUseCase {
     });
 
     const carsWithAvailability = await Promise.all(transformedCategories);
+
+    if (data.brand || data.type) {
+      const filteredCategories = carsWithAvailability
+        .map((category) => ({
+          ...category,
+          cars: category.cars.filter((car) => {
+            return (
+              (!data.brand || car.brand === data.brand) &&
+              (!data.type || category.type === data.type)
+            );
+          }),
+        }))
+        .filter((category) => {
+          return category.cars.length > 0;
+        });
+
+      return filteredCategories;
+    }
 
     return carsWithAvailability;
   }
