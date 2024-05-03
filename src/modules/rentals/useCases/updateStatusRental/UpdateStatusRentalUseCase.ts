e8c3753a8741a89820e5624c85a9f91f4e80d67e
lmorais <lmorais.contato@gmail.com>
2024-05-03 15:54:38 -0300

    feat: implement update status of rental

---
+++ src/modules/rentals/useCases/updateStatusRental/UpdateStatusRentalUseCase.ts
@@ -0,0 +1,27 @@
+import { inject } from 'tsyringe';
+
+import { RentalStatus } from '@modules/rentals/dtos/enums/RentatStatus';
+import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
+import { IRentalRepository } from '@modules/rentals/repositories/IRentalRepository';
+import { AppError } from '@shared/errors/AppError';
+
+export class UpdateStatusRentalUseCase {
+  constructor(
+    @inject('RentalRepository')
+    private rentalRepository: IRentalRepository
+  ) {}
+
+  async execute(id: string): Promise<Rental> {
+    const rental = await this.rentalRepository.findById(id);
+
+    if (!rental) {
+      throw new AppError('Rental not found', 404);
+    }
+
+    Object.assign(rental, {
+      status: RentalStatus.CONFIRMED,
+    });
+
+    return this.rentalRepository.save(rental);
+  }
+}
