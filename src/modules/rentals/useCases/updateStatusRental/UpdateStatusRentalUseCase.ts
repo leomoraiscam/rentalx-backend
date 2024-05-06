b6dc2e333225667c7031b6cebc743d2eb22fb1aa
lmorais <lmorais.contato@gmail.com>
2024-05-06 20:16:40 -0300

    chore: bkp feat

--- src/modules/rentals/useCases/updateStatusRental/UpdateStatusRentalUseCase.ts
+++ src/modules/rentals/useCases/updateStatusRental/UpdateStatusRentalUseCase.ts
@@ -1,16 +1,23 @@
+import path from 'path';
 import { inject, injectable } from 'tsyringe';
 
+import { IUserRepository } from '@modules/accounts/repositories/IUserRepository';
 import { RentalStatus } from '@modules/rentals/dtos/enums/RentatStatus';
 import { IUpdateStatusRentalDTO } from '@modules/rentals/dtos/IUpdateStatusRentalUseCaseDTO';
 import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 import { IRentalRepository } from '@modules/rentals/repositories/IRentalRepository';
+import { IMailProvider } from '@shared/container/providers/MailProvider/models/IMailProvider';
 import { AppError } from '@shared/errors/AppError';
 
 @injectable()
 export class UpdateStatusRentalUseCase {
   constructor(
     @inject('RentalRepository')
-    private rentalRepository: IRentalRepository
+    private rentalRepository: IRentalRepository,
+    @inject('UserRepository')
+    private userRepository: IUserRepository,
+    @inject('MailProvider')
+    private mailProvider: IMailProvider
   ) {}
 
   async execute({ id, status }: IUpdateStatusRentalDTO): Promise<Rental> {
@@ -28,6 +35,44 @@ export class UpdateStatusRentalUseCase {
     Object.assign(rental, {
       status: statusToSave,
     });
+
+    if (status === RentalStatus.CONFIRMED) {
+      const templatePath = path.resolve(
+        __dirname,
+        '..',
+        '..',
+        'views',
+        'emails',
+        'create-rental.hbs'
+      );
+
+      const { name, email } = await this.userRepository.findById(rental.userId);
+
+      await this.mailProvider.sendMail<{
+        name: string;
+        code: string;
+        startDate: string;
+        startHour: string;
+        expectReturnDate: string;
+        expectReturnHour: string;
+        dailies: number;
+        total: number;
+      }>({
+        path: templatePath,
+        subject: 'Confirmação de reserva',
+        to: email,
+        variables: {
+          name,
+          code: rental.id,
+          startDate: '',
+          startHour: '',
+          expectReturnDate: '',
+          expectReturnHour: '',
+          dailies: 0,
+          total: 0,
+        },
+      });
+    }
 
     return this.rentalRepository.save(rental);
   }
