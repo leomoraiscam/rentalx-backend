12907aa8b6be693410fc2742206a114a1e4a8aa1
lmorais <lmorais.contato@gmail.com>
2024-05-10 18:34:05 -0300

    fix: add exception to car in create images car use case

--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -2,18 +2,28 @@ import { inject, injectable } from 'tsyringe';
 
 import { IUploadCarImagesDTO } from '@modules/cars/dtos/IUploadCarImagesDTO';
 import { ICarImageRepository } from '@modules/cars/repositories/ICarImageRepository';
+import { ICarRepository } from '@modules/cars/repositories/ICarRepository';
 import { IStorageProvider } from '@shared/container/providers/StorageProvider/models/IStorageProvider';
+import { AppError } from '@shared/errors/AppError';
 
 @injectable()
 export class UploadCarImageUseCase {
   constructor(
     @inject('CarImageRepository')
     private carsImageRepository: ICarImageRepository,
+    @inject('CarRepository')
+    private carRepository: ICarRepository,
     @inject('StorageProvider')
     private storageProvider: IStorageProvider
   ) {}
 
   async execute({ carId, imagesName }: IUploadCarImagesDTO): Promise<void> {
+    const car = await this.carRepository.findById(carId);
+
+    if (!car) {
+      throw new AppError('Car not found', 404);
+    }
+
     const values = imagesName.map(async (image) => {
       await this.carsImageRepository.create({ carId, imageName: image });
       await this.storageProvider.save(image, 'cars');
