edd897b5aee68b242070dd287f6a47398d194ef5
lmorais <lmorais.contato@gmail.com>
2024-08-18 12:09:14 -0300

    feat: use csv stream parser in import categories usecase

--- src/modules/cars/useCases/importSpecifications/ImportSpecificationsUseCase.ts
+++ src/modules/cars/useCases/importSpecifications/ImportSpecificationsUseCase.ts
@@ -1,9 +1,8 @@
-import csvParse from 'csv-parse';
-import fs from 'fs';
 import { injectable, inject } from 'tsyringe';
 
 import { IImportCategoriesDTO } from '@modules/cars/dtos/IImportCategoriesDTO';
 import { ISpecificationRepository } from '@modules/cars/repositories/ISpecificationRepository';
+import { ICSVStreamParserProvider } from '@shared/container/providers/CSVStreamParserProvider/models/ICSVStreamParserProvider';
 
 type IImportSpecificationsDTO = Omit<IImportCategoriesDTO, 'type'>;
 
@@ -11,42 +10,19 @@ type IImportSpecificationsDTO = Omit<IImportCategoriesDTO, 'type'>;
 export class ImportSpecificationsUseCase {
   constructor(
     @inject('SpecificationRepository')
-    private specificationRepository: ISpecificationRepository
+    private specificationRepository: ISpecificationRepository,
+    @inject('CSVStreamParserProvider')
+    private CSVStreamParserProvider: ICSVStreamParserProvider
   ) {}
 
-  loadSpecifications(
-    file: Express.Multer.File
-  ): Promise<IImportSpecificationsDTO[]> {
-    return new Promise((resolve, reject) => {
-      const stream = fs.createReadStream(file.path);
-      const specifications: IImportSpecificationsDTO[] = [];
-      const parseFile = csvParse();
-
-      stream.pipe(parseFile);
-      parseFile
-        .on('data', async (line) => {
-          const [name, description] = line;
-
-          specifications.push({
-            name,
-            description,
-          });
-        })
-        .on('end', () => {
-          fs.promises.unlink(file.path);
-
-          resolve(specifications);
-        })
-        .on('error', (error) => {
-          reject(error);
-        });
-    });
-  }
-
   async execute(file: Express.Multer.File): Promise<void> {
-    const specifications = await this.loadSpecifications(file);
+    const keys = ['name', 'description'];
+    const specifications = await this.CSVStreamParserProvider.parse<IImportSpecificationsDTO>(
+      file.path,
+      keys
+    );
 
-    specifications.map(async (data) => {
+    const specificationsPromises = specifications.map(async (data) => {
       const { name, description } = data;
       const specification = await this.specificationRepository.findByName(name);
 
@@ -57,5 +33,7 @@ export class ImportSpecificationsUseCase {
         });
       }
     });
+
+    await Promise.all(specificationsPromises);
   }
 }
