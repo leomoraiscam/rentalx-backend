af4d42ff122fa454eb20a6ca33cd6368bbaf45e4
lmorais <lmorais.contato@gmail.com>
2024-08-18 12:10:04 -0300

    feat: use csv stream parser in import specifications usecase

--- src/modules/cars/useCases/importCategories/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategories/ImportCategoryUseCase.ts
@@ -1,49 +1,26 @@
-import csvParse from 'csv-parse';
-import fs from 'fs';
 import { injectable, inject } from 'tsyringe';
 
 import { IImportCategoriesDTO } from '@modules/cars/dtos/IImportCategoriesDTO';
 import { ICategoryRepository } from '@modules/cars/repositories/ICategoryRepository';
+import { ICSVStreamParserProvider } from '@shared/container/providers/CSVStreamParserProvider/models/ICSVStreamParserProvider';
 
 @injectable()
 export class ImportCategoryUseCase {
   constructor(
     @inject('CategoryRepository')
-    private categoryRepository: ICategoryRepository
+    private categoryRepository: ICategoryRepository,
+    @inject('CSVStreamParserProvider')
+    private CSVStreamParserProvider: ICSVStreamParserProvider
   ) {}
 
-  loadCategories(file: Express.Multer.File): Promise<IImportCategoriesDTO[]> {
-    return new Promise((resolve, reject) => {
-      const stream = fs.createReadStream(file.path);
-      const parseFile = csvParse();
-      const categories: IImportCategoriesDTO[] = [];
-
-      stream.pipe(parseFile);
-      parseFile
-        .on('data', async (line) => {
-          const [name, description, type] = line;
-
-          categories.push({
-            name,
-            description,
-            type,
-          });
-        })
-        .on('end', () => {
-          fs.promises.unlink(file.path);
-
-          resolve(categories);
-        })
-        .on('error', (error) => {
-          reject(error);
-        });
-    });
-  }
-
   async execute(file: Express.Multer.File): Promise<void> {
-    const categories = await this.loadCategories(file);
+    const keys = ['name', 'description', 'type'];
+    const categories = await this.CSVStreamParserProvider.parse<IImportCategoriesDTO>(
+      file.path,
+      keys
+    );
 
-    categories.map(async (data) => {
+    const categoriesPromises = categories.map(async (data) => {
       const { name, description, type } = data;
       const category = await this.categoryRepository.findByName(name);
 
@@ -55,5 +32,7 @@ export class ImportCategoryUseCase {
         });
       }
     });
+
+    await Promise.all(categoriesPromises);
   }
 }
