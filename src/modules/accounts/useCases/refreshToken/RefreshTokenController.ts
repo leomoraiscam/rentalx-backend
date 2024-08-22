7d7f5ef7a33c4a5287122383a3f8849891d91b13
lmorais <lmorais.contato@gmail.com>
2024-08-22 12:29:07 -0300

    chore: get token only request body

--- src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
@@ -5,10 +5,8 @@ import { RefreshTokenUseCase } from './RefreshTokenUseCase';
 
 export class RefreshTokenController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const token =
-      request.body.token ||
-      request.headers['x-access-token'] ||
-      request.query.token;
+    const { token } = request.body;
+
     const refreshTokenUseCase = container.resolve(RefreshTokenUseCase);
     const refreshToken = await refreshTokenUseCase.execute(token);
 
