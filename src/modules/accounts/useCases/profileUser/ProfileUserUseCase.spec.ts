e490eec2a8ae8af825d3c4261264e26433f96d7b
lmorais <lmorais.contato@gmail.com>
2024-08-22 21:58:16 -0300

    test: add user profile

---
+++ src/modules/accounts/useCases/profileUser/ProfileUserUseCase.spec.ts
@@ -0,0 +1,34 @@
+import { InMemoryUserRepository } from '../../repositories/in-memory/InMemoryUserRepository';
+import { ProfileUserUseCase } from './ProfileUserUseCase';
+
+describe('ProfileUserUseCase', () => {
+  let inMemoryUserRepository: InMemoryUserRepository;
+  let profileUserUseCase: ProfileUserUseCase;
+
+  beforeEach(() => {
+    inMemoryUserRepository = new InMemoryUserRepository();
+    profileUserUseCase = new ProfileUserUseCase(inMemoryUserRepository);
+  });
+
+  it('should be able to show user profile when user exist', async () => {
+    const { id: userId } = await inMemoryUserRepository.create({
+      name: 'Jennie McCoy',
+      email: 'hiv@metja.lk',
+      password: 'any-pass123',
+      driverLicense: '5304286925',
+      avatar: 'fake-avatar',
+      isAdmin: false,
+    });
+
+    const profile = await profileUserUseCase.execute(userId);
+
+    expect(profile).toEqual({
+      id: userId,
+      name: 'Jennie McCoy',
+      email: 'hiv@metja.lk',
+      driverLicense: '5304286925',
+      avatar: 'fake-avatar',
+      avatarUrl: 'http://localhost:3333/avatar/fake-avatar',
+    });
+  });
+});
