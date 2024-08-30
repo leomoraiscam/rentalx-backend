2e5039e0d8cf394c23e2c6f5789774386acc0369
lmorais <lmorais.contato@gmail.com>
2024-08-29 22:56:44 -0300

    feat: added rental date service in update rental use case

--- src/modules/rentals/useCases/updateRental/UpdateRentalUseCase.ts
+++ src/modules/rentals/useCases/updateRental/UpdateRentalUseCase.ts
@@ -3,23 +3,26 @@ import { inject, injectable } from 'tsyringe';
 import { ICarRepository } from '@modules/cars/repositories/ICarRepository';
 import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 import { IRentalRepository } from '@modules/rentals/repositories/IRentalRepository';
+import { IRentalDateService } from '@modules/rentals/services/IRentalDateService';
 import { IDateProvider } from '@shared/container/providers/DateProvider/models/IDateProvider';
 import { AppError } from '@shared/errors/AppError';
 
 @injectable()
 export class UpdateRentalUseCase {
+  private MINIMUM_HOURS = 24;
+
   constructor(
     @inject('RentalRepository')
     private rentalRepository: IRentalRepository,
     @inject('CarRepository')
     private carRepository: ICarRepository,
     @inject('DateProvider')
-    private dateProvider: IDateProvider
+    private dateProvider: IDateProvider,
+    @inject('RentalDateService')
+    private rentalDateService: IRentalDateService
   ) {}
 
   async execute(data: Partial<Rental>): Promise<Rental> {
-    const minimumHours = 24;
-    let total = 0;
     const car = await this.carRepository.findById(data.carId);
 
     if (!car) {
@@ -33,80 +36,40 @@ export class UpdateRentalUseCase {
     }
 
     if (data.startDate || data.expectedReturnDate) {
-      const dateNow = await this.dateProvider.dateNow();
-      const isBefore = await this.dateProvider.compareIfBefore(
-        data.startDate,
-        dateNow
-      );
+      this.rentalDateService.validateStartDate(data.startDate);
+      this.rentalDateService.validateRentalHours(data.startDate);
+      this.rentalDateService.validateRentalHours(data.expectedReturnDate);
 
-      if (isBefore) {
-        throw new AppError('You cant create an rental on a past date', 422);
-      }
-
-      const startDateIsLess8AmHour = this.dateProvider.getHours(data.startDate);
-      const startDateIsMore18PmHour = this.dateProvider.getHours(
-        data.startDate
-      );
-
-      if (
-        Number(startDateIsLess8AmHour.substring(0, 2)) < 8 ||
-        Number(startDateIsMore18PmHour.substring(0, 2)) > 18
-      ) {
-        throw new AppError('You can only create rental 8am and 18pm', 422);
-      }
-
-      const returnDateIsLess8AmHour = this.dateProvider.getHours(
-        data.expectedReturnDate
-      );
-      const returnDateIsMore18PmHour = this.dateProvider.getHours(
-        data.expectedReturnDate
-      );
-
-      if (
-        Number(returnDateIsLess8AmHour.substring(0, 2)) < 8 ||
-        Number(returnDateIsMore18PmHour.substring(0, 2)) > 18
-      ) {
-        throw new AppError(
-          'You can only devolution car between 8am and 18pm',
-          422
-        );
-      }
-
-      const compare = this.dateProvider.compareInHours(
+      const rentalDurationInHours = this.dateProvider.compareInHours(
         data.startDate,
         data.expectedReturnDate
       );
 
-      if (compare < minimumHours) {
+      if (rentalDurationInHours < this.MINIMUM_HOURS) {
         throw new AppError('Invalid return time!', 422);
       }
 
-      const dailies = await this.dateProvider.compareInDays(
+      rental.total = this.rentalDateService.calculateTotal(
+        rental.car,
         data.startDate,
         data.expectedReturnDate
       );
-
-      total = dailies * rental.car.dailyRate;
-      rental.total = total;
     }
 
     if (data.carId && rental.carId !== data.carId) {
-      const dailies = await this.dateProvider.compareInDays(
+      const car = await this.carRepository.findById(data.carId);
+
+      rental.total = this.rentalDateService.calculateTotal(
+        car,
         rental.startDate,
         rental.expectedReturnDate
       );
-      const car = await this.carRepository.findById(data.carId);
-
-      total = dailies * car.dailyRate;
-      rental.total = total;
     }
 
     Object.assign(rental, {
       ...data,
     });
 
-    const updatedRental = await this.rentalRepository.save(rental);
-
-    return updatedRental;
+    return this.rentalRepository.save(rental);
   }
 }
