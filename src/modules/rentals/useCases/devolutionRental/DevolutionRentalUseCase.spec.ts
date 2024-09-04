7c306426601f35b0d8f9b3bd23b3ea7fbea911ff
lmorais <lmorais.contato@gmail.com>
2024-09-04 09:27:01 -0300

    feat: improve assertions in devolution rental

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.spec.ts
@@ -1,5 +1,3 @@
-import dayjs from 'dayjs';
-
 import { InMemoryCarRepository } from '@modules/cars/repositories/in-memory/InMemoryCarRepository';
 import { InMemoryDateProvider } from '@shared/container/providers/DateProvider/in-memory/InMemoryDateProvider';
 import { AppError } from '@shared/errors/AppError';
@@ -12,10 +10,9 @@ describe('DevolutionRentalUseCase', () => {
   let inMemoryCarRepository: InMemoryCarRepository;
   let inMemoryDateProvider: InMemoryDateProvider;
   let devolutionRentalUseCase: DevolutionRentalUseCase;
+  let carId: string;
 
-  const dayAdd24Hours = dayjs().add(1, 'day').toDate();
-
-  beforeEach(() => {
+  beforeEach(async () => {
     inMemoryRentalRepository = new InMemoryRentalRepository();
     inMemoryCarRepository = new InMemoryCarRepository();
     inMemoryDateProvider = new InMemoryDateProvider();
@@ -24,14 +21,7 @@ describe('DevolutionRentalUseCase', () => {
       inMemoryCarRepository,
       inMemoryDateProvider
     );
-  });
-
-  it('should be able to devolve a rental', async () => {
-    jest.spyOn(Date, 'now').mockImplementation(() => {
-      return new Date(2024, 3, 8).getTime();
-    });
-
-    const { id: carId } = await inMemoryCarRepository.create({
+    const { id } = await inMemoryCarRepository.create({
       name: 'A3',
       brand: 'Audi',
       description: 'Car test',
@@ -48,115 +38,100 @@ describe('DevolutionRentalUseCase', () => {
         },
       ],
     });
+    carId = id;
+  });
 
-    const rental = await inMemoryRentalRepository.create({
+  it('should be able to return an devolution rental when received correct data', async () => {
+    jest.spyOn(inMemoryDateProvider, 'dateNow').mockImplementationOnce(() => {
+      return new Date(2024, 3, 8);
+    });
+
+    const { id } = await inMemoryRentalRepository.create({
       userId: 'fake-user-id',
       carId,
       status: 'confirmed',
-      expectedReturnDate: dayAdd24Hours,
+      expectedReturnDate: new Date(2024, 3, 11, 12),
       startDate: new Date(2024, 3, 10, 12),
     });
-
-    const updatedRental = await devolutionRentalUseCase.execute({
-      id: rental.id,
+    const rental = await devolutionRentalUseCase.execute({
+      id,
     });
 
-    expect(updatedRental).toHaveProperty('endDate');
+    expect(rental).toHaveProperty('endDate');
+    expect(rental.endDate).toEqual(new Date(2024, 3, 8));
   });
 
-  it('should not be able to devolve a rental when the same a non exist', async () => {
-    await expect(
-      devolutionRentalUseCase.execute({ id: 'a-non-exist-rental' })
-    ).rejects.toBeInstanceOf(AppError);
-  });
-
-  it('should not be able to devolve a rental when the same a non exist', async () => {
-    jest.spyOn(Date, 'now').mockImplementation(() => {
-      return new Date(2024, 3, 8).getTime();
+  it('should be able to return an devolution rental with total property when ocurred in less 24 hours', async () => {
+    jest.spyOn(inMemoryDateProvider, 'dateNow').mockImplementationOnce(() => {
+      return new Date(2024, 3, 3, 9);
     });
 
-    const rental = await inMemoryRentalRepository.create({
-      userId: 'fake-user-id',
-      carId: 'fake-car-id',
-      expectedReturnDate: dayAdd24Hours,
-      startDate: new Date(2024, 3, 10, 12),
-    });
-
-    await expect(
-      devolutionRentalUseCase.execute({ id: rental.id })
-    ).rejects.toBeInstanceOf(AppError);
-  });
-
-  it('should be able to set devolution rental to the same day when ocurred in less 24 hours', async () => {
-    const dateNow = inMemoryDateProvider.dateNow();
-    const dayAdd2Hours = inMemoryDateProvider.addHours(2);
-
-    const { id: carId } = await inMemoryCarRepository.create({
-      name: 'A3',
-      brand: 'Audi',
-      description: 'Car test',
-      dailyRate: 100,
-      licensePlate: 'AJN-730',
-      fineAmount: 80,
-      categoryId: 'executive',
-      specifications: [
-        {
-          id: 'fake-id',
-          createdAt: new Date(),
-          description: 'fake-description',
-          name: 'fake-name',
-        },
-      ],
-    });
-
-    const rental = await inMemoryRentalRepository.create({
+    const { id } = await inMemoryRentalRepository.create({
       userId: 'fake-user-id',
       carId,
-      startDate: dateNow,
-      expectedReturnDate: dayAdd2Hours,
+      startDate: new Date(2024, 3, 3, 10),
+      expectedReturnDate: new Date(2024, 3, 3, 12),
       status: 'confirmed',
     });
-
     const devolutionRental = await devolutionRentalUseCase.execute({
-      id: rental.id,
+      id,
     });
 
     expect(devolutionRental.total).toEqual(100);
   });
 
-  it('should be able to set devolution rental to 1 days after', async () => {
-    const dayAdd2Hours = inMemoryDateProvider.addHours(2);
-
-    const { id: carId } = await inMemoryCarRepository.create({
-      name: 'A3',
-      brand: 'Audi',
-      description: 'Car test',
-      dailyRate: 100,
-      licensePlate: 'AJN-730',
-      fineAmount: 80,
-      categoryId: 'executive',
-      specifications: [
-        {
-          id: 'fake-id',
-          createdAt: new Date(),
-          description: 'fake-description',
-          name: 'fake-name',
-        },
-      ],
+  it('should be able to return an devolution rental when ocurred 1 days after', async () => {
+    jest.spyOn(inMemoryDateProvider, 'dateNow').mockImplementationOnce(() => {
+      return new Date(2024, 3, 3, 9);
     });
 
-    const rental = await inMemoryRentalRepository.create({
+    const { id } = await inMemoryRentalRepository.create({
       userId: 'fake-user-id',
       carId,
-      expectedReturnDate: dayAdd24Hours,
-      startDate: dayAdd2Hours,
+      expectedReturnDate: new Date(2024, 3, 4, 11),
+      startDate: new Date(2024, 3, 3, 11),
       status: 'confirmed',
     });
-
     const devolutionRental = await devolutionRentalUseCase.execute({
-      id: rental.id,
+      id,
     });
 
     expect(devolutionRental.total).toEqual(180);
+  });
+
+  it('should not be able to return an devolution rental when the same a non-exist', async () => {
+    await expect(
+      devolutionRentalUseCase.execute({ id: 'a-non-exist-rental' })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to return an devolution rental when status is different of confirmed', async () => {
+    jest.spyOn(Date, 'now').mockImplementation(() => {
+      return new Date(2024, 3, 8).getTime();
+    });
+
+    const { id } = await inMemoryRentalRepository.create({
+      userId: 'fake-user-id',
+      carId,
+      expectedReturnDate: new Date(2024, 3, 11, 12),
+      startDate: new Date(2024, 3, 10, 12),
+    });
+    await expect(
+      devolutionRentalUseCase.execute({ id })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to return an devolution rental when car a non-exist', async () => {
+    const { id } = await inMemoryRentalRepository.create({
+      userId: 'fake-user-id',
+      carId: 'faked-car',
+      expectedReturnDate: new Date(2024, 3, 11, 12),
+      startDate: new Date(2024, 3, 10, 12),
+      status: 'confirmed',
+    });
+
+    await expect(
+      devolutionRentalUseCase.execute({ id })
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
