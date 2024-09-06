f5abca46c623b17c609b96ec42d2d4c974a21a1e
lmorais <lmorais.contato@gmail.com>
2024-09-06 10:20:27 -0300

    feat: improve and define rental status in update rental use case

--- src/modules/rentals/useCases/updateRental/UpdateRentalUseCase.ts
+++ src/modules/rentals/useCases/updateRental/UpdateRentalUseCase.ts
@@ -1,6 +1,8 @@
 import { inject, injectable } from 'tsyringe';
 
+import { CarStatus } from '@modules/cars/enums/CarStatus';
 import { ICarRepository } from '@modules/cars/repositories/ICarRepository';
+import { RentalStatus } from '@modules/rentals/enums/RentatStatus';
 import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 import { IRentalRepository } from '@modules/rentals/repositories/IRentalRepository';
 import { IRentalDateService } from '@modules/rentals/services/IRentalDateService';
@@ -23,16 +25,17 @@ export class UpdateRentalUseCase {
   ) {}
 
   async execute(data: Partial<Rental>): Promise<Rental> {
-    const car = await this.carRepository.findById(data.carId);
-
-    if (!car) {
-      throw new AppError('Car not found', 404);
-    }
-
     const rental = await this.rentalRepository.findById(data.id);
 
     if (!rental) {
       throw new AppError('Rental not found', 404);
+    }
+
+    if (!rental.status.includes(RentalStatus.CONFIRMED)) {
+      throw new AppError(
+        'The rent cannot be updated as it has passed the period',
+        422
+      );
     }
 
     if (data.startDate || data.expectedReturnDate) {
@@ -59,11 +62,37 @@ export class UpdateRentalUseCase {
     if (data.carId && rental.carId !== data.carId) {
       const car = await this.carRepository.findById(data.carId);
 
-      rental.total = this.rentalDateService.calculateTotal(
+      if (!car) {
+        throw new AppError('Car not found', 404);
+      }
+
+      if (!car.status.includes(CarStatus.AVAILABLE)) {
+        throw new AppError('This car is not available', 422);
+      }
+
+      const currentCar = await this.carRepository.findById(rental.carId);
+
+      if (currentCar) {
+        currentCar.status = CarStatus.AVAILABLE;
+
+        await this.carRepository.save(currentCar);
+      }
+
+      car.status = CarStatus.RESERVED;
+
+      await this.carRepository.save(car);
+
+      const total = this.rentalDateService.calculateTotal(
         car,
         rental.startDate,
         rental.expectedReturnDate
       );
+
+      Object.assign(rental, {
+        carId: car.id,
+        car,
+        total,
+      });
     }
 
     Object.assign(rental, {
