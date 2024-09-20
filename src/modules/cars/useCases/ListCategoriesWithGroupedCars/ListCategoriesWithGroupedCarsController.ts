698882fae2f3aa6a4f6b33a561e4df42ffa220a7
lmorais <lmorais.contato@gmail.com>
2024-09-19 22:24:56 -0300

    fix: get categoryId in request and send to use case

--- src/modules/cars/useCases/ListCategoriesWithGroupedCars/ListCategoriesWithGroupedCarsController.ts
+++ src/modules/cars/useCases/ListCategoriesWithGroupedCars/ListCategoriesWithGroupedCarsController.ts
@@ -7,7 +7,13 @@ import { ListCategoriesWithGroupedCarsUseCase } from './ListCategoriesWithGroupe
 
 export class ListCategoriesWithGroupedCarsController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { brand, type, startDate, expectedReturnDate } = request.query;
+    const {
+      brand,
+      type,
+      startDate,
+      expectedReturnDate,
+      categoryId,
+    } = request.query;
     const listCategoriesCarsGroupUseCase = container.resolve(
       ListCategoriesWithGroupedCarsUseCase
     );
@@ -16,6 +22,7 @@ export class ListCategoriesWithGroupedCarsController {
       type: type as CategoryType,
       startDate: new Date(startDate as string),
       expectedReturnDate: new Date(expectedReturnDate as string),
+      categoryId: categoryId as string,
     });
 
     return response.status(200).json(cars);
